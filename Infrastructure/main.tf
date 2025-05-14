1f1810257d54f34a96cc689318dee71bcd55a693
lexicoder1 <uzorifeanyi1000@gmail.com>
2025-05-14 19:31:53 +0100

    update

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -11,45 +11,38 @@ module "myvpc1" {
   createnatgateway = var.createnatgateway      
 }
 
-# module "my-cluster1" {
-# source = "./Modules/Eks"
-# ekscluster-role-name = var.ekscluster-role-name
-# myekscluster-nodegroup-role-name = var.myekscluster-nodegroup-role-name
-# Env = var.Env
-# subnet_ids = module.myvpc1.private_subnet_ids
-# cluster-name = var.cluster-name
-# node_group_name= var.node_group_name
-# arn_iamuser_access_to_cluster = var.arn_iamuser_access_to_cluster  
-# }
+module "my-cluster1" {
+source = "./Modules/Eks"
+ekscluster-role-name = var.ekscluster-role-name
+myekscluster-nodegroup-role-name = var.myekscluster-nodegroup-role-name
+Env = var.Env
+subnet_ids = module.myvpc1.private_subnet_ids
+cluster-name = var.cluster-name
+node_group_name= var.node_group_name
+arn_iamuser_access_to_cluster = var.arn_iamuser_access_to_cluster  
+}
 
 
-# module "my-jenkins-Ec2" {
-# source = "./Modules/Ec2"
-# key_name = var.jenkins_key_name
-# public_key = var.jenkins_public_key
-# ami = var.jenkins_ami
-# instance_type= var.jenkins_instance_type
-# Ec2_name = var.jenkins_Ec2_name
-# volume_size = var.jenkins_volume_size
-# subnet_id  = module.myvpc1.public_subnet_ids[0]
-# Env  = var.Env
-# vpc_id = module.myvpc1.vpc_id-1
-# security_group_name = var.jenkins_security_group_name
-# inbound_configurations = var.jenkins_inbound_configurations
-# }
+module "my-jenkins-Ec2" {
+source = "./Modules/Ec2"
+key_name = var.jenkins_key_name
+public_key = var.jenkins_public_key
+ami = var.jenkins_ami
+instance_type= var.jenkins_instance_type
+Ec2_name = var.jenkins_Ec2_name
+volume_size = var.jenkins_volume_size
+subnet_id  = module.myvpc1.public_subnet_ids[0]
+Env  = var.Env
+vpc_id = module.myvpc1.vpc_id-1
+security_group_name = var.jenkins_security_group_name
+inbound_configurations = var.jenkins_inbound_configurations
+}
 
-# output "publicip" {
-#   value = module.my-jenkins-Ec2.publicip
-# }
+output "publicip" {
+  value = module.my-jenkins-Ec2.publicip
+}
 
-# module "my-s3-bucket" {
-# source = "./Modules/S3" 
-# Env =var.Env
-# bucket_name =var.bucket_name
-# files =var.files
-# upload_files = var.upload_files
-# enable_bucket_versioning = var.enable_bucket_versioning
-# }
+
 
 module "my-rds" {
 source = "./Modules/Rds-Mariadb" 
